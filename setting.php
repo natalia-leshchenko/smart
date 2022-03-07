3d4d9ff28a7502f807d6020cbb5975d7bcbe38e2
natalia-leshchenko <100552085+natalia-leshchenko@users.noreply.github.com>
2022-03-07 22:36:41 +0600

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,6 +1,6 @@
 <?php
-define ('HOST', 'a282431.mysql.mchost.ru');
-define ('USER', 'a282431_test55');
-define ('PASS', '89836236299Zx');
-define('DB', 'a282431_test55');
+define ('HOST', '*******');
+define ('USER', '*******');
+define ('PASS', '*******');
+define('DB', '********');
 ?>
